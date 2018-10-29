9334c6a58ae3fb04a3e0c684d6eceb31009e7eb5
Joshua Sullivan <joshua.sullivan@spok.com>
2018-10-29 09:21:24 -0500

    Setting initial project parameters.

--- challenge-01-animation/ViewController.swift
+++ challenge-01-animation/ViewController.swift
@@ -9,12 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var button: UIButton!
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBAction private func startAnimation() {
+        // Animation code goes here.
     }
-
 
 }
 
