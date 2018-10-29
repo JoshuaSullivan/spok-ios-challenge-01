e0dfe31a2a3b6ef32ef384dbb3b1dad0f29a33b6
Joshua Sullivan <joshua.sullivan@spok.com>
2018-10-29 08:41:22 -0500

    Initial Commit

---
+++ challenge-01-animation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  challenge-01-animation
+//
+//  Created by Joshua.Sullivan on 10/29/18.
+//  Copyright © 2018 Joshua.Sullivan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
